ca62bfbcc05fe0a1213aaf3e458d261b2800af89
Jeni1336 <jeniffer1336@gmail.com>
2025-08-25 17:41:33 -0300

    atualizando

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+
+$dbHost = 'Localhost';
+$dbUsername = 'root';
+$dbPassword = '';
+$dbName = 'formulario-teste';
+
+
+$conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+
+//if ($conexao->connect_error)
+//{
+//    echo "Erro";
+//}
+//else{
+  //  echo "Conexão efetuada com sucesso";
+//}
+?>
